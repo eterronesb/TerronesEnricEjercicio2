eafe350ef37df3458466facc682f9f61e8494bf4
eterronesb <sucir1993@gmail.com>
2024-12-08 19:00:05 +0100

    Comenzamos el main y realizamos el bucle del menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    //lista para almacenar eventos
+    private static ArrayList<Event> events = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int option = 0;
+        //bucle principarl del menu
+        while (option != 5) {
+            showMenu();//muestra el menu
+            option = Integer.parseInt(scanner.nextLine());
+            switch (option) {
+                case 1:
+                    addEvent();
+                    break;
+                case 2:
+                    deleteEvent();
+                    break;
+                case 3:
+                    listEvent();
+                    break;
+                case 4:
+                    togleTaskCompletion();
+                    break;
+                case 5:
+                    System.out.println("Adios!");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+            }
+        }
     }
+
 }
