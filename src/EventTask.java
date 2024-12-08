3be722580c9957fcebdc966f684f62362dfe1939
eterronesb <sucir1993@gmail.com>
2024-12-08 17:29:51 +0100

    Creacion del objeto Event Task

---
+++ src/EventTask.java
@@ -0,0 +1,25 @@
+public class EventTask {
+    //atributos
+    private String text;
+    private boolean isCompleted;
+
+    //constructor
+    public EventTask(String text) {
+        this.text = text;
+        this.isCompleted = false;
+    }
+    //metodo para marcar o desmarcar
+    public void toggleCompletion() {
+        isCompleted = !isCompleted;
+    }
+    //estado de la tarea
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+    //metodo toString para mostrar detalles de la tarea
+
+    public String toString() {
+        return (isCompleted ? "[Completada] " : "[Pendiente]") + text;
+    }
+}
